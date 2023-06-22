adca1fc3b9598449f242aa9e49208a481fb36a0d
Matias Uriel Zuber Schachner <83487@sistemas.frc.utn.edu.ar>
2023-06-22 11:47:12 -0300

    Correcion Pantalla

--- src/Components/PantallaConsultarEncuesta.js
+++ src/Components/PantallaConsultarEncuesta.js
@@ -1,24 +1,21 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import gestor from "../models/GestorConsultarEncuesta.js";
 import "./PantallaConsultarEncuesta.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
+
 function PantallaConsultarEncuesta() {
     const [ventana, setVentana] = useState(false)
     const [periodo, setPeriodo] = useState(false)
+    const [encuestas, setEncuestas] = useState(false)
 
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
     const [startDateOk, setStartDateOk] = useState(false);
     const [llamadaSeleccionada, setLlamadaSeleccionada] = useState(false)
+
     const [cancel, setCancel] = useState(false)
-    
-    function opcionConsultarEncuesta(){
-        if (ventana === false) {
-            habilitarVentana()
-        }
-    }
 
     function Inicio(){
         return (
@@ -29,28 +26,64 @@ function PantallaConsultarEncuesta() {
             </button>
         )
     }
-   
-    function habilitarVentana() {
-        return setVentana(true)
+    
+    function opcionConsultarEncuesta(){
+        if (ventana === false) {
+            habilitarVentana()
+        }
     }
 
-    function pedirPeriodoFecha(){
-        setPeriodo(true)
+    function habilitarVentana() {
+        return setVentana(true)
     }
 
     function NuevaConsulta(){
         return (
             <div className="buttonContainer">
                 <button onClick={() => {
-                    if (gestor.nuevaConsultaEncuesta() === true) {
-                        pedirPeriodoFecha()
-                    } else {
-                        window.alert("Debe Resetear la busqueda para iniciar una nueva consulta")
-                    }
+                        setPeriodo(gestor.nuevaConsultaEncuesta())
                     }} className="btn btn-primary p-2 m-5">Nueva Consulta</button>
             </div>
         )
     }
+
+    function pedirPeriodoFecha(){
+        return (
+            <div className="d-flex flex-row m-1 justify-content-around">
+                <div className="d-flex flex-column align-items-center">
+                    <form onSubmit={tomarFechaInicio}>
+                        <div className="fecha">
+                            <label for="start-date">Fecha de inicio:</label>
+                            <input type="date" id="start-date" name="start-date"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)
+                            } 
+                            />
+                        </div>
+                        <input type="submit" value="Cargar Inicio" 
+                        className="btn btn-outline-success"/>
+                    </form>
+                    <h6 className="p-2">Fecha Inicio: {startDate}</h6>
+                </div>
+                {startDateOk &&
+                <div className="d-flex flex-column align-items-center">
+                    <form onSubmit={tomarFechaFin}>
+                        <div className="fecha">
+                            <label for="end-date">Fecha fin:</label>
+                            <input type="date" id="end-date" name="end-date"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)} />
+                        </div>
+                        <input type="submit" value="Cargar Fin" 
+                        className="btn btn-outline-success"/>
+                    </form>
+                    <h6 className="p-2">Fecha Fin: {endDate}</h6>
+                </div>
+                }
+            </div>
+        )
+    }
+
 
 
     const tomarFechaInicio = (e) => {
@@ -69,14 +102,15 @@ function PantallaConsultarEncuesta() {
         } else {
             console.log(endDate)
             console.log('Formulario enviado');
-            setVentana(false)
+            setEncuestas(true)
+            setLlamadaSeleccionada(false)
+            gestor.llamadaSeleccionada = null
         }
     };
 
     function guardarLlamadas(){
         gestor.tomarPeriodoFecha(startDate, endDate)
         gestor.buscarLlamadasConEncuestas();
-        // console.log(gestor.llamadasEncuesta)
         return true
     }
 
@@ -85,17 +119,28 @@ function PantallaConsultarEncuesta() {
         setLlamadaSeleccionada(true)
     }
 
+    function BotonCancelar(){
+        return (
+            <footer className="footer p-2 py-4">
+                <div className="Btn-container d-flex justify-content-end">
+                    <button onClick={() => {cancelar()}} className="btn btn-primary p-3 m-5">Cancelar</button>
+                    {cancel && window.confirm("¿Desea cancelar la consulta?")}
+                </div>  
+            </footer>
+            )
+    }
+
     function mostrarLlamadasConEncuesta(){
         if (gestor.llamadasEncuesta.length === 0){
             return (
-                <div>
+                <div className="mx-auto">
                     <h3>No hay llamadas con encuestas en el periodo seleccionado</h3>
                 </div>
             )
         }
         
         return (
-            <table className="table table-striped table-hover">
+            <table className="table table-striped table-hover mx-auto">
                 <thead>
                     <td><strong>Nombre del Cliente</strong></td>
                     <td><strong>Estado Actual</strong></td>
@@ -104,7 +149,7 @@ function PantallaConsultarEncuesta() {
                     {gestor.llamadasEncuesta.map((llamada, index) => (
                         <tr key={index} onClick={() => tomarSeleccionLlamada(llamada)}>
                             <td>{llamada.cliente.getNombre()}</td>
-                            <td>{llamada.encuestaEnviada.getDescripcionEncuesta()}</td>
+                            <td>{llamada.getDuracion()}</td>
                         </tr>))}
                 </tbody>
             </table>       
@@ -124,72 +169,86 @@ function PantallaConsultarEncuesta() {
         let datos = gestor.obtenerDatosLlamadaSeleccionada()
         return (
             <div>
-                <table className="table table-striped table-hover">
+                <table className="table table-striped table-hover mx-auto">
                     <thead>
                         <tr>
                             <td><strong>Cliente</strong></td>
                             <td><strong>Estado Actual</strong></td>
                             <td><strong>Duracion</strong></td>
                         </tr>
-                        {/* <td><strong>Respuestas Seleccionadas</strong></td>
-                        <td><strong>Descripcion</strong></td>
-                        <td><strong>Pregunta</strong></td> */}
                     </thead>
                     <tbody>
                         <tr>
-                            <td>{datos.datosLlamada[0]}</td>
-                            <td>{datos.datosLlamada[1]}</td>
-                            <td>{datos.datosLlamada[2]}</td>
+                            <td>{datos.datosLlamada.cliente}</td>
+                            <td>{datos.datosLlamada.estado}</td>
+                            <td>{datos.datosLlamada.duracion}</td>
                         </tr>
                     </tbody>
                 </table>
-                <h3>{datos.encuesta}</h3>
-                <table className="table table-striped table-hover">
+                <table className="table table-striped table-hover mx-auto">
                     <thead>
-                        <td>Pregunta 1</td>
-                        <td>Pregunta 2</td>
-                        <td>Pregunta 3</td>
+                        <tr>
+                            <td><strong>Descripcion Encuesta</strong></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{datos.encuesta}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className="table table-striped table-hover mx-auto">
+                    <thead>
+                        <td><strong>Pregunta 1</strong></td>
+                        <td><strong>Pregunta 2</strong></td>
+                        {datos.preguntas[2] &&
+                            <td><strong>Pregunta 3</strong></td>
+                        }
                     </thead>
                     <tbody>
                         <tr>
                             <td>{datos.preguntas[0]}</td>
                             <td>{datos.preguntas[1]}</td>
+                            {datos.preguntas[2] &&
                             <td>{datos.preguntas[2]}</td>
+                            }
                         </tr>
                         <tr>
                             <td>{datos.respuestaCliente[0]}</td>
                             <td>{datos.respuestaCliente[1]}</td>
+                            {datos.respuestaCliente[2] &&
                             <td>{datos.respuestaCliente[2]}</td>
+                            }
                         </tr>
 
                     </tbody>
                 </table>
                 {pedirSeleccionFormaVisualizacion()}
-                {/* <div className="Btn-container">
-                    <button onClick={() => {pedirSeleccionFormaVisualizacion()}} className="btn btn-primary p-2 m-5">Descargar CSV</button>
-                </div> */}
             </div>
         )
     }
 
     function tomarSeleccionFormaVisualizacion(){
-        // return tomarSeleccionFormaVisualizacion()
         return gestor.tomarFormaVisualizacion()
     }
 
     function confirmCancelar() {
         setVentana(false)
         setPeriodo(false)
+        setEncuestas(false)
         setStartDateOk(false)
         setLlamadaSeleccionada(false)
         setCancel(false)
         setStartDate("")
         setEndDate("")
-        opcionConsultarEncuesta()
+        gestor.llamadaSeleccionada = null
     }
 
     function cancelar(){
-        setCancel(true)
+        let opc = window.confirm("¿Desea cancelar la consulta?")
+        if (opc === true){
+            confirmCancelar()
+        }
     }
     
     return (
@@ -198,61 +257,24 @@ function PantallaConsultarEncuesta() {
                 <h1>Consultar Llamadas</h1>
             </header>
             {!ventana && Inicio()}
-            {ventana && <div className="container">
-            {!periodo && NuevaConsulta()
-            // <div className="buttonContainer">
-            //     <button onClick={() => {
-            //         if (gestor.nuevaConsultaEncuesta() === true) {
-            //             pedirPeriodoFecha()
-            //         } else {
-            //             window.alert("Debe Resetear la busqueda para iniciar una nueva consulta")
-            //         }
-            //         }} className="btn btn-primary p-2 m-5">Nueva Consulta</button>
-            // </div>
-            }
-            {periodo && <div>
-                {!startDateOk && 
-                    <div>
-                        <form onSubmit={tomarFechaInicio}>
-                           <div className="fecha">
-                               <label for="start-date">Fecha de inicio:</label>
-                               <input type="date" id="start-date" name="start-date"
-                               value={startDate}
-                               onChange={(e) => setStartDate(e.target.value)
-                                } 
-                               />
-                           </div>
-                           <input type="submit" value="Cargar Fecha Inicio" />
-                        </form>
-                            <h6>Fecha Inicio: {startDate}</h6>
+            
+            {ventana && <div className="container p-2 py-4 mx-auto main-container">
+            
+                {!periodo && NuevaConsulta()}
+
+                {periodo &&  <div>
+                    {pedirPeriodoFecha()}
+
+                    {encuestas && guardarLlamadas() && <div className="mx-auto">
+                        {!llamadaSeleccionada && mostrarLlamadasConEncuesta()}
+                        {llamadaSeleccionada && mostrarDatosLlamadaSeleccionada()}
                     </div>
-                }
-                {startDateOk &&
-                <form onSubmit={tomarFechaFin}>
-                    <div className="fecha">
-                        <label for="end-date">Fecha fin:</label>
-                        <input type="date" id="end-date" name="end-date"
-                        value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)} />
-                    </div>
-                    <input type="submit" value="Cargar Fecha Fin" />
-                </form>
-                }
-            </div>        
-            }
-        </div>
-            }
-            {!ventana && setStartDateOk && guardarLlamadas() && <div>
-                {!llamadaSeleccionada && mostrarLlamadasConEncuesta()}
-                {llamadaSeleccionada && mostrarDatosLlamadaSeleccionada()}
-                </div>}
-            <footer>
-                <div className="Btn-container">
-                    <button onClick={() => {cancelar()}} className="btn btn-primary p-2 m-5">Cancelar</button>
-                    {cancel && 
-                    <button onClick={() => {confirmCancelar()}} className="btn btn-primary p-2 m-5">Confirmar Cancelacion</button>}
+                    }
                 </div>
-            </footer>
+                    }
+            </div>
+            }
+            {ventana && BotonCancelar()}
         </div>
     );
 }
